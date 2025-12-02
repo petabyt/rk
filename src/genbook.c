d4446069a15a5c36b19ca2f644440d54b056d7f0
Daniel C <brikbusters@gmail.com>
2025-12-02 14:34:09 -0500

    tweaks, fix typos, add more fw handlers to pinebook/genbook
    Former-commit-id: db34fd55ab25c17e8de4e7f99ef23923fe14de10

--- src/genbook.c
+++ src/genbook.c
@@ -7,10 +7,20 @@
 static uint8_t *shared_mem;
 
 uint64_t plat_process_firmware_call(uint64_t p1, uint64_t p2, uint64_t p3, uint64_t p4) {
-	struct FuScreenList *screens = (void *)(shared_mem);
-	struct FuMmioDeviceList *ohci = (void *)(shared_mem + 0x40);
-	struct FuMmioDeviceList *gic = (void *)(shared_mem + (0x40 * 2));
-	struct FuMemoryMap *map = (void *)(shared_mem + (0x40 * 3));
+	uint8_t *buf = shared_mem;
+	struct FuScreenList *screens = (void *)(buf);
+	buf += 0x40;
+	struct FuMmioDeviceList *ohci = (void *)(buf);
+	buf += 0x40;
+	struct FuMmioDeviceList *i2c = (void *)(buf);
+	buf += 0x40;
+	struct FuMmioGic *gic = (void *)(buf);
+	buf += 0x40;
+	struct FuDeviceInfo *info = (void *)(buf);
+	buf += 0x40;
+	struct FuI2cDeviceList *i2cd = (void *)(buf);
+	buf += 0x80;
+	struct FuMemoryMap *map = (void *)(buf);
 	switch (p1) {
 	case FU_GET_SCREEN_LIST:
 		screens->length = 1;
@@ -25,15 +35,32 @@ uint64_t plat_process_firmware_call(uint64_t p1, uint64_t p2, uint64_t p3, uint6
 		return (uintptr_t)&map->items[2];
 	case FU_GET_OHCI_LIST:
 		ohci->length = 1;
-		ohci->devices[0].address = 0xfe3a0000;
+		ohci->devices[0].address = USB2HOST_0_OHCI;
 		ohci->devices[0].n_interrupts = 0;
 		return (uintptr_t)ohci;
 	case FU_GET_MEM_MAP:
 		plat_get_mem_map(map);
 		return (uintptr_t)map;
 	case FU_GET_GIC:
-		gic->length = 0;
+		gic->exists = 0;
 		return (uintptr_t)gic;
+	case FU_GET_RKI2C_LIST:
+		i2c->length = 1;
+		i2c->devices[0].address = I2C4;
+		return (uintptr_t)i2c;
+	case FU_GET_I2C_SLAVES:
+		if (p2 == I2C4) {
+			i2cd->length = 1;
+			i2cd->devices[0].address = 0x62;
+			strcpy(i2cd->devices[0].dtb_compatible, "cellwise,cw2015");
+		} else {
+			i2cd->length = 0;
+		}
+		return (uintptr_t)i2cd;
+	case FU_GET_DEVICE_INFO:
+		strcpy(info->vendor, "Cool-Pi");
+		strcpy(info->product, "Genbook");
+		return (uintptr_t)info;
 	}
 
 	return FU_ERROR;
@@ -67,9 +94,9 @@ int c_entry(void) {
 	rk3588_sgrf_init();
 	rk3588_init_power_domains();
 
-	rk3588_setup_video_edp1(0xd0000000, 1920, 1080);
+	rk3588_setup_video_edp1(plat_get_framebuffer(), 1920, 1080);
 
-	uint8_t buffer[500];
+	uint8_t buffer[1000];
 	shared_mem = buffer;
 
 	jump_to_payload();
